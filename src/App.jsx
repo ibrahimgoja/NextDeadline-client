68b3f505e65860f2a449ff8c8bf6b1e0f7eacb1d
ibrahimgoja <ibrahemgoja@gmail.com>
2026-05-16 13:42:33 +0300

    feat: setup role-based routing and navigation structure and removed Notification feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,50 @@
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 import Header from './components/Header'
+
+import StudentDashboard from "./pages/student/StudentDashboard"
+import Semesters from "./pages/student/Semesters"
+import Calendar from "./pages/student/Calendar"
+import KanbanBoard from "./pages/student/KanbanBoard"
+import StudentSettings from "./pages/student/StudentSettings"
+
+import InstructorDashBoard from "./pages/instructor/InstructorDashboard"
+import Courses from "./pages/instructor/Courses"
+import Students from "./pages/instructor/Students"
+import Grades from "./pages/instructor/Grades"
+import InstructorSettings from "./pages/instructor/InstructorSettings"
 import "./css/Reset.css"
 import "./App.css"
 
 function App() {
 
+  const Role = "Student";
+
   return (
     <>
-    <Router>
-      <Header></Header>
-      <main className='main-content'>
-      <Routes>
-
-      </Routes>
-      </main>
-    </Router>
+      <Router>
+        <Header Role={Role} />
+        <main className='main-content'>
+          <Routes>
+            {Role == "Student" ? (
+              <>
+                <Route path='/Student/Dashboard' element={<StudentDashboard />} />
+                <Route path='/Student/Semesters' element={<Semesters />} />
+                <Route path='/Student/Calendar' element={<Calendar />} />
+                <Route path='/Student/KanbanBoard' element={<KanbanBoard />} />
+                <Route path='/Student/Settings' element={<StudentSettings />} />
+              </>
+            ) : (
+              <>
+                <Route path='/Instructor/Dashboard' element={<InstructorDashBoard />} />
+                <Route path="/Instructor/Courses" element={<Courses />} />
+                <Route path="/Instructor/Students" element={<Students />} />
+                <Route path="/Instructor/Grades" element={<Grades />} />
+                <Route path="/Instructor/Settings" element={<InstructorSettings />} />
+              </>
+            )}
+          </Routes>
+        </main>
+      </Router>
 
 
     </>
---
+++ src/pages/instructor/InstructorSettings.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const InstructorSettings = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default InstructorSettings;
